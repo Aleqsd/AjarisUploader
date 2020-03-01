f803371d14fd1e73d91c6bc87900cf1348a69583
Alex <aleqsd@gmail.com>
2020-03-01 18:32:07 +0100

    Show demo notification with progressbar

--- app/src/main/java/com/example/ajarisuploader/UploadActivity.java
+++ app/src/main/java/com/example/ajarisuploader/UploadActivity.java
@@ -1,17 +1,23 @@
 package com.example.ajarisuploader;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
-import android.os.Parcelable;
 import android.util.Log;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 /***
  * Activité visée à s'ouvrir par l'intent du share
@@ -26,24 +32,71 @@ public class UploadActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         TextView textView = findViewById(R.id.urisTextView);
 
-
         Intent intent = getIntent();
-        if (intent.getParcelableExtra("URI") != null)
-        {
+        if (intent.getParcelableExtra("URI") != null) {
             Uri uri = intent.getParcelableExtra("URI");
-            textView.setText(uri.getEncodedPath());
-        }
-        else if (intent.getParcelableArrayListExtra("URI") != null)
-        {
+            textView.setText(Objects.requireNonNull(uri).getEncodedPath());
+            showDemoNotification();
+        } else if (intent.getParcelableArrayListExtra("URI") != null) {
             StringBuilder urisString = new StringBuilder();
             ArrayList<Uri> uris = intent.getParcelableArrayListExtra("URI");
-            for (Uri uri : uris)
+            for (Uri uri : Objects.requireNonNull(uris))
                 urisString.append(uri.getEncodedPath());
             textView.setText(urisString);
-        }
-        else
-            Log.e("IntentError","L'intent reçu est invalide");
+            showDemoNotification();
+        } else
+            Log.e("IntentError", "L'intent reçu est invalide");
 
+    }
+
+    private void showDemoNotification() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "channel_name";
+            String description = "channel_description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("Ajaris", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            Objects.requireNonNull(notificationManager).createNotificationChannel(channel);
+        }
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, UploadActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "Ajaris")
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setContentTitle("Ajaris Upload")
+                .setContentText("Upload in progress...")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        // Issue the initial notification with zero progress
+        int PROGRESS_MAX = 100;
+        int PROGRESS_CURRENT = 50;
+        builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
+        notificationManager.notify(1, builder.build());
+
+        // Do the job here that tracks the progress.
+        // Usually, this should be in a
+        // worker thread
+        // To show progress, update PROGRESS_CURRENT and update the notification with:
+        // builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
+        // notificationManager.notify(notificationId, builder.build());
+
+        //// When done, update the notification one more time to remove the progress bar
+        //        builder.setContentText("Download complete")
+        //                .setProgress(0,0,false);
+
+        notificationManager.notify(1, builder.build());
     }
 
 }
