3bef028b98df32f58db87b828a8d3d7b7291f294
Cadrew <adrien.canino@ynov.com>
2020-03-02 12:47:43 +0100

    Add Profils Page

---
+++ app/src/main/java/com/example/ajarisuploader/About.java
@@ -0,0 +1,36 @@
+package com.example.ajarisuploader;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class About extends Fragment {
+
+    private AboutViewModel mViewModel;
+
+    public static About newInstance() {
+        return new About();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.about_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(AboutViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/ajarisuploader/AboutViewModel.java
@@ -0,0 +1,7 @@
+package com.example.ajarisuploader;
+
+import androidx.lifecycle.ViewModel;
+
+public class AboutViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
